a9535a4908c7e74cac45a542ca97cdd4b93c9bae
Soren's Coding Land <chaz.com@hotmail.co.uk>
2017-12-05 20:53:23 +0000

    Ahoy Matey - Terrain, Skybox, Water created & Ship added.
    I spent most of today and some of yesterday working on the game.
    I will continue on Friday. This will take some time but I want to finish it.

---
+++ Ahoy Matey/Assets/Players/PlayerNetwork.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class PlayerNetwork : NetworkBehaviour {
+
+    private Vector3 inputValue; // What? Why? How? 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) // How does this help or stop the bellow code? I'll never answer this, will I.
+        {
+            return;
+        }
+
+            inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            inputValue.y = 0f;
+            inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+            transform.Translate(inputValue);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+            GetComponentInChildren<Camera>().enabled = true; // I don't know.
+    }
+
+}
