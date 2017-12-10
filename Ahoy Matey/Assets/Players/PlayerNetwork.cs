c28bcb59a9dc217191a738a37c1ccfa3735657e2
Soren's Coding Land <chaz.com@hotmail.co.uk>
2017-12-10 19:13:15 +0000

    Ahoy Matey - Ship Controls improved
    I have made it so that the ship turns like a 17th century ship does. I also made it so that the ship could not reverse because that made no sense.
    It also works over LAN and the two audio listner error has been fixed.
    The next step is to make the sky box not horrible to look at when turning the ship or just swap it out for another. Then I'll create, animate and code the cannon balls.

--- Ahoy Matey/Assets/Players/PlayerNetwork.cs
+++ Ahoy Matey/Assets/Players/PlayerNetwork.cs
@@ -8,24 +8,48 @@ public class PlayerNetwork : NetworkBehaviour {
 
     private Vector3 inputValue; // What? Why? How? 
 
+    public float rotationSpeed = 1F;
+    public float speed = 10.0F;
+
+    void Start()
+    {
+        // deactivate the CharacterController that is not Mine in the Network 
+      //  if (!Network.isMine)
+     //   {
+          //  GetComponent<CharacterController>().enabled = false;
+       //     GetComponentInChildren<AudioListener>().enabled = false;
+       //     GetComponentInChildren<Camera>().enabled = false;
+       // }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (!isLocalPlayer) // How does this help or stop the bellow code? I'll never answer this, will I.
         {
+         //   GetComponent<CharacterController>().enabled = false;
+          //  GetComponentInChildren<AudioListener>().enabled = false;
+          //  GetComponentInChildren<Camera>().enabled = false;
             return;
         }
 
-            inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+           // rotation *= Time.deltaTime;  // This was causing it to not rotate? Not sure why.
+            transform.Rotate(0, rotation, 0);
+            // inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
             inputValue.y = 0f;
-            inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+            //inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+            float translation = Input.GetAxis("Vertical") * speed;
+            translation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
 
-            transform.Translate(inputValue);
+        transform.Translate(inputValue);
     }
 
     public override void OnStartLocalPlayer()
     {
-            GetComponentInChildren<Camera>().enabled = true; // I don't know.
+            GetComponentInChildren<Camera>().enabled = true; // It makes the localplayer enable the camera, which stops it from having two in the game window.
+            GetComponentInChildren<AudioListener>().enabled = true;
     }
 
 }
