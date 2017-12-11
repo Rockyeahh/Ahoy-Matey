b8d8ea81ff8001ffec0b70fd0e85ef310ce460fb
Soren's Coding Land <chaz.com@hotmail.co.uk>
2017-12-11 20:03:32 +0000

    Ahoy Matey - The projectiles have been created
    They have an error where they don't shoot off in a direction. They used to but they wouldn't fire in the correct direction when you turned the ship. Once that is fixed I can move onto getting it colliding with another ship and doing damage, taking away health and destroying itself after impact.

---
+++ Ahoy Matey/Assets/Projectiles/CannonBallRight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallRight : MonoBehaviour {
+
+    public float speed, damage;
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rigidBody.velocity = transform.right * speed * Time.deltaTime; // Starboard is right.
+        print("Fire right");
+    }
+}
---
+++ Ahoy Matey/Assets/Projectiles/FireCannons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCannons : MonoBehaviour {
+
+    public GameObject cannonBallRight, cannonBallLeft, cannonBallHolder;
+
+    private GameObject projectileParent;
+
+    // Use this for initialization
+    void Start () {
+        projectileParent = GameObject.Find("projectiles");
+        if (projectileParent == null)
+        {
+            projectileParent = new GameObject("projectiles");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) // Left
+        {
+            Fire1();
+        }
+
+        if (Input.GetButtonDown("Fire2")) // Right
+        {
+            Fire2();
+        }
+    }
+
+    private void Fire1()
+    {
+        GameObject newProjectile2 = Instantiate(cannonBallLeft) as GameObject;
+        newProjectile2.transform.parent = projectileParent.transform;
+        newProjectile2.transform.position = cannonBallHolder.transform.position;
+    }
+
+    private void Fire2()
+    {
+        GameObject newProjectile1 = Instantiate(cannonBallRight) as GameObject;
+        newProjectile1.transform.parent = projectileParent.transform;
+        newProjectile1.transform.position = cannonBallHolder.transform.position;
+    }
+
+}
